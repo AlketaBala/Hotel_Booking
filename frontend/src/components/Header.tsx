45831595d82f7819574d7db8eb14daf3a287f272
Maltine <maltina.rama@gmail.com>
2024-03-27 20:57:05 +0100

    Header Implementation #1

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 const Header = () => {
   return (
-    <div className="bg-blue-800 py-6">
+    <div className="bg-tan-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to="/">MernHolidays.com</Link>
+          <Link to="/">Derand Hotel</Link>
         </span>
         <span className="flex space-x-2">
             <>
               <Link
-                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                className="flex items-center text-white px-3 font-bold hover:bg-tan-600"
                 to="/my-bookings"
               >
                 My Bookings
               </Link>
               <Link
-                className="flex items-center text-white px-3 font-bold hover:bg-blue-600"
+                className="flex items-center text-white px-3 font-bold hover:bg-tan-600"
                 to="/my-hotels"
               >
                 My Hotels
@@ -23,7 +23,7 @@ const Header = () => {
             </>
             <Link
               to="/sign-in"
-              className="flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100"
+              className="flex bg-white items-center text-tan-600 px-3 font-bold hover:bg-gray-100"
             > Sign In
             </Link>
         </span>
