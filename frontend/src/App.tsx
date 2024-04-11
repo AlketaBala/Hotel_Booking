73e1a1b5e7f2c824848ec9f5e5f567f1f68b47db
Maltine <maltina.rama@gmail.com>
2024-04-11 20:10:32 +0200

    #4

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./Pages/Register";
 
 const App = () => {
   return (
     <Router>
       <Routes>
             <Route path="/" element={<Layout></Layout>} />
-            <Route path="/search" element={<>Search Page</>} />
+            <Route path="/register" element={
+              <Layout>
+                 <Register/>
+</Layout>
+} />
 
             <Route path="*" element={<Navigate to="/" />} />
       </Routes>
