a2fcd9bdd337a968177e7a14087d1b6210ecccf7
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2024-04-12 17:40:05 +0200

    #13

--- backend/src/migrations/migration-v2-create-phonenumber.ts
+++ backend/src/migrations/migration-v2-create-phonenumber.ts
@@ -6,36 +6,9 @@ const up = async (): Promise<void> => {
     useUnifiedTopology: true,
   });
 
-  const UserSchema = new mongoose.Schema({
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    phoneNumber: { type: String },
-  });
+  const User = mongoose.model('User', new mongoose.Schema({}, { strict: false, collection: 'users' }));
 
-  await mongoose.connection.collection('users').createIndex({ email: 1 }, { unique: true });
-
-  await mongoose.model('User', UserSchema).create({
-    email: 'Alketa.Bala@gmail.com',
-    password: 'password123',
-    firstName: 'Alketa',
-    lastName: 'Bala',
-    phoneNumber: '38349231654',
-  });
+  await User.collection.updateMany({}, { $set: { phoneNumber: null } });
 
   await mongoose.connection.close();
 };
-const down = async (): Promise<void> => {
-  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
-
-  await mongoose.model('User', new mongoose.Schema({}, { strict: false, collection: 'users' })).deleteMany({});
-  await mongoose.connection.collection('users').dropIndex('email_1');
-
-  await mongoose.connection.close();
-};
-
-export { up, down };
