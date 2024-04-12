f8c3686f1d1708dc3b6e0d1e21dbf588de43bcaa
Jeta-Syla <117122719+Jeta-Syla@users.noreply.github.com>
2024-04-12 19:13:44 +0200

    CreatePhoneNumberMigration #13

--- backend/src/migrations/migration-v2-create-phonenumber.ts
+++ backend/src/migrations/migration-v2-create-phonenumber.ts
@@ -12,3 +12,17 @@ const up = async (): Promise<void> => {
 
   await mongoose.connection.close();
 };
+const down = async (): Promise<void> => {
+  await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const User = mongoose.model('User', new mongoose.Schema({}, { strict: false, collection: 'users' }));
+
+  await User.collection.updateMany({}, { $unset: { phoneNumber: 1 } });
+
+  await mongoose.connection.close();
+};
+
+export { up, down };
