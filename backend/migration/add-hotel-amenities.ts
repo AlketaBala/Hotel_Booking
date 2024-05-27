0f50631e76b26d9b1c6ecad6005bc0d43c3535d4
Maltine Rama <94307059+MaltineR@users.noreply.github.com>
2024-05-28 00:00:22 +0200

    #49

--- backend/migration/add-hotel-amenities.ts
+++ backend/migration/add-hotel-amenities.ts
@@ -1,9 +1,9 @@
 import { Db } from "mongodb";
 
 export const up = async (db: Db) => {
-  await db.collection("hotels").updateMany({}, { $set: { amenities: [] } });
+  await db.collection("hotels").createIndex({ name: "text" });
 };
 
 export const down = async (db: Db) => {
-  await db.collection("hotels").updateMany({}, { $unset: { amenities: 1 } });
+  await db.collection("hotels").dropIndex("name_text"); 
 };
