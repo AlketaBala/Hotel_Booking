3b2e062ff0b774a8c686f0f3ab94cda461efd8eb
AlketaBala <93828279+AlketaBala@users.noreply.github.com>
2024-05-27 16:05:30 +0200

    Fixed Issues

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -4,7 +4,7 @@ const Header = () => {
     <div className="bg-green-800 py-6">
       <div className="container mx-auto flex justify-between">
         <span className="text-3xl text-white font-bold tracking-tight">
-          <Link to="/">Derand Hotel</Link>
+          <Link to="/">HotelBooking</Link>
         </span>
         <span className="flex space-x-2">
             <>
